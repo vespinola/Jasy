a2810ebe66870590c619a7a03aacdf2d1cb048ac
Vladimir Espinola Lezcano <vladimir.espinola@gmail.com>
2018-02-11 17:26:45 -0300

    firebase and fabric are funcional

--- Jasy/AppDelegate.swift
+++ Jasy/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 import Fabric
 import Crashlytics
-
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Fabric.with([Crashlytics.self])
+        FirebaseApp.configure()
         AppDelegate.stack?.autoSave(30)
         return true
     }
